508a4d994a14453148609e1e6b8d76785efd5bb7
Rahul <rahulbodne2018@gmail.com>
2025-03-24 22:05:22 +0530

    this is 1st commit from new updated laptop

---
+++ src/main/java/newPractice/practiceDemo/Controller/rahul.java
@@ -0,0 +1,9 @@
+package newPractice.practiceDemo.Controller;
+
+public class rahul {
+
+    public static void main(String[] args) {System.out.println("this is m 1st code please ignoe");
+
+        System.out.println("jioweioweoij");
+    }
+}
