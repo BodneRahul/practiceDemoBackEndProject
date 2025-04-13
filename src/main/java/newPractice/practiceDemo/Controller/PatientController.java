839f229255e1e05a8f881aacd6f2db43ed640a2e
chetangitdemo <chetanbodne7294@gmail.com>
2025-04-13 19:43:34 +0530

    Controller

--- src/main/java/newPractice/practiceDemo/Controller/PatientController.java
+++ src/main/java/newPractice/practiceDemo/Controller/PatientController.java
@@ -47,6 +47,7 @@ public class PatientController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePatient(@PathVariable Long id) {
         patientService.deletePatient(id);
+        System.out.println(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
